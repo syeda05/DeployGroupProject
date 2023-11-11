c41783c6ced1dedf80d688abac1b2f992c20c816
FatimaFarhan425 <149039711+FatimaFarhan425@users.noreply.github.com>
2023-11-11 11:19:59 +0300

    Added the recipe class

---
+++ main.py
@@ -0,0 +1,7 @@
+class Recipe:
+    def __init__(self, recipeName, ingredients, instructions, category, rating  ):
+         self.recipeName = recipeName
+         self.ingredients = ingredients
+         self.instruction = instructions
+         self.category =category
+         self.rating = rating
