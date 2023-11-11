ec121fc83b754d44383e9e6658c1f7dfbe11256b
syeda05 <60101544@udst.edu.qa>
2023-11-11 12:24:02 +0300

    Add RMS class with the db connection

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
 class Recipe:
     def __init__(self, recipeName, ingredients, instructions, category, rating  ):
          self.recipeName = recipeName
@@ -5,3 +8,12 @@ class Recipe:
          self.instruction = instructions
          self.category =category
          self.rating = rating
+
+class RecipeManagmentSystem:
+    def _init_(self, db_name = 'recipes'):
+        cred = credentials.Certificate("key.json")
+        firebase_admin.initialize_app(cred)
+        self.db = firestore.client()
+        self.collection = self.db.collection(db_name)
+
+r = RecipeManagmentSystem()
