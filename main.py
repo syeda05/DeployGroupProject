88cfe612529ec1f17dfbcf4d6f8de9fcd74f45d7
FatimaFarhan425 <149039711+FatimaFarhan425@users.noreply.github.com>
2023-11-13 22:05:22 +0300

    Delete functions added with validation

--- main.py
+++ main.py
@@ -37,13 +37,32 @@ class RecipeManagmentSystem:
             elif userInput == '3':
                 print("Function for Editing Recipe will be called")
             elif userInput == '4':
-                print("Function for Deleting Recipe will be called")
+                userInput=input("Enter the ID number of the record you want to delete :")
+                self.deleteRecipe(userInput)
             elif userInput == '5':
                 print("Function for Exiting Recipe will be called")
                 
         else:
             print("-----------------------------------------------")
             print("Error: The number should be between 1 to 5 inclusively")
+            self.selectOptions()
+    def deleteRecipe(self, userInput):
+        
+        while not self.collection.document(userInput).get().exists:
+             print("The record with that ID doesn't exist")
+             check=input("Do you want to select another record ID for deleteing (Type yes or no)?")
+             if check.lower()=='yes':
+                userInput=input("Enter the ID number of the record you want to delete :")
+             else:
+                self.selectOptions()
+               
+        verification= input("Are you sure you want to delete the record (Type yes or no)?")
+        if verification.lower()=='yes':
+             self.collection.document(userInput).delete()
+             print('The record has been deleted sucessfully')
+             self.selectOptions()
+             
+        else:
             self.selectOptions()
 
 r = RecipeManagmentSystem()
