1879fceb70a8a8307a024f6f5d579dfb720a660c
syeda05 <60101544@udst.edu.qa>
2023-11-22 16:57:21 +0300

    Added test for editRecipe

--- test_recipe_management_integration.py
+++ test_recipe_management_integration.py
@@ -1,8 +1,10 @@
 import unittest
+from unittest.mock import MagicMock, patch
 import firebase_admin
 from firebase_admin import credentials, firestore
 # from main import RecipeManagementSystem  # Assuming RecipeManagementSystem is in main.py
 import main 
+
 class TestRecipeManagementSystem(unittest.TestCase):
 
     def test_delete_recipe(self):
@@ -22,6 +24,24 @@ class TestRecipeManagementSystem(unittest.TestCase):
         updated = [doc.to_dict() for doc in ref]
 
         self.assertNotIn(recipe_added, updated, msg="Recipe should not be in the list")
+    
+    @patch('main.RecipeManagmentSystem.editRecipe.input', create=True)   #creates a MagicMock() object
+    def test_editRecipe(self, mock_editRecipe):
+        management = main.RecipeManagmentSystem()
+        r=main.Recipe("12", "Brownie", 'potato, salt, pepper', 'gather, cut, cook', 'Lunch', '5')
+        management.addRecipe(r)
+
+        mock_editRecipe.side_effects = [1,'Fries','yes']    
+
+        management.editRecipe('62')
+        # mock_editRecipe.assert_called_once_with('62', option=1, name='Fries', verification='yes')
+        allrecipes = management.collection.get()
+        final_data = [doc.to_dict() for doc in allrecipes]
+
+        recipe_dic ={"id":62,"name": "Fries", "ingredient": "chicken, salt", "instruction": 'gsgssgsgs', 'category': 'Lunch', 'rating': '5'}
+        
+        self.assertIn(recipe_dic, final_data)
+
 
 if __name__ =='__main__':
     unittest.main()
