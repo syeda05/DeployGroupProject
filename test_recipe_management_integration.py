20a9629eefb9f8f89a1ef439af827623655ebfed
FatimaFarhan425 <149039711+FatimaFarhan425@users.noreply.github.com>
2023-11-23 10:46:41 +0300

    patch aadded for test_delete_recipe function

--- test_recipe_management_integration.py
+++ test_recipe_management_integration.py
@@ -1,21 +1,31 @@
 import unittest
 import firebase_admin
 from firebase_admin import credentials, firestore
+from unittest.mock import patch
 # from main import RecipeManagementSystem  # Assuming RecipeManagementSystem is in main.py
 import main 
 class TestRecipeManagementSystem(unittest.TestCase):
-
-    def test_delete_recipe(self):
+    @patch('builtins.input', side_effect=['Yes', 'No'])
+    def test_delete_recipe(self, mock_input):
+        # Your test logic here
         management = main.RecipeManagmentSystem()
-        r=main.Recipe("12", "Soup", 'potato, salt, pepper', 'gather, cut, cook', 'Lunch', '5')
+        r = main.Recipe("12", "Soup", 'potato, salt, pepper', 'gather, cut, cook', 'Lunch', '5')
         management.addRecipe(r)
-        recipe_added = {"id": '12', "name": "Soup", "ingredient": 'potato, salt, pepper',
-                        "instructions": 'gather, cut, cook', 'category': 'Lunch', 'rating': '5'}
-        management.deleteRecipe('12')
+        recipe_added = {
+            "id": '12',
+            "name": "Soup",
+            "ingredient": 'potato, salt, pepper',
+            "instructions": 'gather, cut, cook',
+            'category': 'Lunch',
+            'rating': '5'
+        }
+        with patch('builtins.print'):
+            management.deleteRecipe('12')
+
         # Updated doc to compare
         reference = management.collection.get()
-        UpdatedList = [doc.to_dict() for doc in reference]
-        self.assertNotIn(recipe_added, UpdatedList)
+        updated_list = [doc.to_dict() for doc in reference]
+        self.assertNotIn(recipe_added, updated_list)
 
     # def test_delete_recipeID_not_present(self):
     #     management = main.RecipeManagmentSystem()
