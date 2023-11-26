25f4851ad620fa915afcfba391b01d47c0faa638
syeda05 <60101544@udst.edu.qa>
2023-11-26 16:31:36 +0300

    Added test_addRecipe function back that was deleted during merge

--- test_recipe_management.py
+++ test_recipe_management.py
@@ -10,6 +10,16 @@ from io import StringIO
 
 
 class TestRecipeManagementSystem(unittest.TestCase):
+    def test_addRecipe(self):
+        management = main.RecipeManagmentSystem()
+        recipe = main.Recipe('13','Smoothie','Avocado, banana, yogurt, tofu, nut butters and chia seeds','Put it all in the blender in the order above. Then blend until it’s as smooth as you like! Pour it in a cup — or a bowl, if you’re a smoothie bowl fan — and slurp it up.','Breakfast','3')
+        recipe_dic ={"id":recipe.id,"name": recipe.recipeName, "ingredient": recipe.ingredients, "instruction": recipe.instructions, 'category': recipe.category, 'rating': recipe.rating}
+
+        management.addRecipe(recipe)
+
+        allrecipes = management.collection.get()
+        final_data = [doc.to_dict() for doc in allrecipes]
+        self.assertIn(recipe_dic, final_data)
     
     def test_view_breakfast_recipes(self):
         management = main.RecipeManagmentSystem()
