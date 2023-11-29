29f87bd2f9835497db0478599b126c554282f149
syeda05 <60101544@udst.edu.qa>
2023-11-29 18:27:14 +0300

    Removed unwanted prints from the tests and clean up

--- test_recipe_management_integration.py
+++ test_recipe_management_integration.py
@@ -6,7 +6,6 @@ from unittest import mock
 from unittest.mock import MagicMock, patch
 import sys
 from io import StringIO
-# from main import RecipeManagementSystem  # Assuming RecipeManagementSystem is in main.py
 import main 
 
 class TestRecipeManagementSystem(unittest.TestCase):
@@ -14,7 +13,9 @@ class TestRecipeManagementSystem(unittest.TestCase):
     def test_delete_recipe(self, mock_input):
         management = main.RecipeManagmentSystem()
         r = main.Recipe("12", "Soup", 'potato, salt, pepper', 'gather, cut, cook', 'Lunch', '5')
-        management.addRecipe(r)
+        
+        with patch('builtins.print'):
+            management.addRecipe(r)
         recipe_added = {
             "id": '12',
             "name": "Soup",
@@ -30,16 +31,16 @@ class TestRecipeManagementSystem(unittest.TestCase):
         reference = management.collection.get()
         updated_list = [doc.to_dict() for doc in reference]
         self.assertNotIn(recipe_added, updated_list)
-
-
     
     @patch('builtins.input', side_effect=['111','1', 'Gelato', 'yes'])  
     def test_editRecipe(self, mock_editRecipe):
         management = main.RecipeManagmentSystem()
         r=main.Recipe("111", "Ice Cream", 'milk, sugar', 'blend together','Lunch', '4')
-        management.addRecipe(r)
+        
+        with patch('builtins.print'):
+            management.addRecipe(r)
 
-        with patch('builtins.print'):  # to avoid unwanted prints during the test
+        with patch('builtins.print'):  
             management.editRecipe('111') 
 
         allrecipes = management.collection.get()
@@ -48,39 +49,25 @@ class TestRecipeManagementSystem(unittest.TestCase):
         recipe_dic ={"id":'111',"name": "Gelato", "ingredient": 'milk, sugar', "instruction": 'blend together', 'category': 'Lunch', 'rating': '4'}
         
         self.assertIn(recipe_dic, final_data)
-
-
     
     def test_view_recipe(self):
         management = main.RecipeManagmentSystem()
         recipe1 = main.Recipe("23", "Juices kiwi", "apple,orange,kiwi", "mix apple and orange, kiwi together", "Dinner", "5")
-        management.addRecipe(recipe1)
+        
+        with patch('builtins.print'):
+            management.addRecipe(recipe1)
 
         with patch('builtins.input', side_effect=['4', 'sys.exit']):
-            with patch('sys.exit'):  # to avoid unwanted prints during the test
+            with patch('sys.exit'): 
                 buffer = StringIO()
                 sys.stdout = buffer
                 management.view_recipe()
                 output = buffer.getvalue()
                 sys.stdout = sys.__stdout__
-
-        
-       
-       
-    
-    
-       
+      
         expected_recipe = {"id":"23", "name ": "Juices kiwi", "ingredint": "apple,orange,kiwi", "instruction":"mix apple and orange, kiwi together",  "category":"Dinner","rating" :"5"}
-        for keys, values in expected_recipe.items():
-            print(values)
-        
-    
-       
-        self.assertIn(values, output)
-
-       
-
-
+        for keys, value in expected_recipe.items():
+            self.assertIn(value, output)
 
 if __name__ =='__main__':
     unittest.main()
